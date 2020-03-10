f34f155a3904294c3db95d9f7f1902eb1ad4d752
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-09 17:24:50 -0700

    update comments for whiteboarding flow

--- challenges/arrayMap/array-map.js
+++ challenges/arrayMap/array-map.js
@@ -1,6 +1,11 @@
 // Write a function called map which takes an array and a function as arguments.
 // Without utilizing any of the built-in methods available to your language, return an array with the function applied to each element.
 
+// Write a function called map that takes a function and an array
+// Initialize the result array
+// Loop through each element in the array
+// Run the function on each element and push the result into the results array
+// Return the results array
 function map(someFunction, array) {
     let resultArray = [];
     for(let i = 0; i < array.length; i++) {
@@ -10,12 +15,17 @@ function map(someFunction, array) {
     return resultArray;
 };
 
+// Example 1: Should add 1 to each number in the array and return [2, 3, 4]
 const numbers = [1, 2, 3];
 const addOne = (argument) => argument + 1;
+console.log(map(addOne, numbers));
 
+// Example 2: Should say hi to each name in the array and return ['Hi, Tommy!', 'Hi, Maxwell!, 'Hi, Sara!']
 const names = ['Tommy', 'Maxwell', 'Sara'];
 const sayHello = (argument) => `Hi, ${argument}!`;
+console.log(map(sayHello, names));
 
+// Example 3: Should return an array of just the cat owners' cats' names (['Loki', 'Bagheera', 'Hobbes', 'Charlotte'])
 const catOwners = [
     {
       name: 'Jenna',
@@ -34,13 +44,5 @@ const catOwners = [
       cat: 'Charlotte'
     }
   ];
-
 const getCats = (object) => object.cat;
-
-// adds one to each number in the array
-console.log(map(addOne, numbers));
-// says 'hi' to each name in the array
-console.log(map(sayHello, names));
-
-// returns just the cat owners' cat names
 console.log(map(getCats, catOwners));
